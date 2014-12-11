2545142ede54e91deeccff8785f89e2d515ae78e
Indy Beak <iwb@cognitivescale.com>
2014-12-11 00:30:06 -0600

    improvement on run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 #
+data=${MONGODB_DATA:-/data}
+rs=${MONGODB_RS:-rs0}
 
-if [ "$(ls -A $MONGODB_DATA)" ];then
+if [ "$(ls -A $data)" ];then
     echo "mongodb data directory is found at " $MONGODB_DATA
 else
     echo "downloading mongodb data... "
-    cd $(dirname $MONGODB_DATA)
+    cd $(dirname $data)
     wget -qO- https://s3.amazonaws.com/c1sandbox/downloads/mongodb/data.v2.tgz | tar zxvf -
 fi
 
 echo "starting mongodb..."
-exec mongod --dbpath $MONGODB_DATA --replSet $MONGODB_RS --rest
+exec mongod --dbpath $data --replSet $rs --rest
