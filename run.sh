1943c0a8c65be33a9b8c29692de00066e792a323
Bill Sidney <bill@cognitivescale.com>
2015-04-07 17:13:52 -0500

    Updated run.sh to create the Replication set instead of download and exploding data.v2.tgz

--- run.sh
+++ run.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
-#
+
 data=${MONGODB_DATA:-/data}
 rs=${MONGODB_RS:-rs0}
 
 if [ "$(ls -A $data)" ];then
-    echo "mongodb data directory is found at " $MONGODB_DATA
+    echo "mongodb data directory is found at " $data
 else
-    echo "downloading mongodb data... "
-    cd $(dirname $data)
-    wget -qO- https://s3.amazonaws.com/c1sandbox/downloads/mongodb/data.v2.tgz | tar zxvf -
+  echo "Creating intial Replication Set "
+
+  nohup mongod --dbpath $data --replSet $rs --rest > /logs/create_replset.log 2> /dev/null &
+  mongod_pid=$!
+
+  while [ `tail  /logs/create_replset.log 2> /dev/null | grep -c 'port 27017'` -eq 0 ];
+  do
+    sleep 1
+  done
+
+  mongo --eval "rs.initiate()" &> /dev/null
+
+  while [ `tail  /logs/create_replset.log 2> /dev/null | grep -c 'replSet PRIMARY'` -eq 0 ];
+  do
+    sleep 1
+  done
+
+  kill $mongod_pid
 fi
 
 echo "starting mongodb..."
